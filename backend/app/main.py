4572ad79ba931a79dfceb98af4cd12ce0f0ae05c
Ronaldo Segundo Junior <ronaldosegundojr@usp.br>
2024-08-27 21:22:17 -0300

    removi o FAST API que estava bugado e voltei a usar o Flask, funcionou!

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
-from api.v1.routes import *
-import uvicorn
+from flask import Flask
 
 """
 Este é o ponto de entrada da aplicação Flask. Ele cria a instância do app e a inicia, configurando o host e a porta.
 Use este arquivo para iniciar a aplicação com `python main.py`.
 """
 
-app = FastAPI()
-app.include_router(api_v1, prefix="/api/v1")
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "E-commerce!"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    app.run(debug=True)
+
 
 
